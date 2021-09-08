53e01303e16f94956c0c7b089859681a1a35e0da
Yuri Frantsevich <frantsevich@gmail.com>
2021-09-08 17:45:07 +0300

    Fixed detect_encoding function for cp1251

--- src/Base.php
+++ src/Base.php
@@ -3,7 +3,7 @@
  * Класс базовых функций
  * @author Yuri Frantsevich (FYN)
  * Date: 17/08/2021
- * @version 1.2.1
+ * @version 1.2.2
  * @copyright 2021
  */
 
@@ -232,8 +232,6 @@ class Base {
             'utf-32be', 'utf-32le', 'utf-16be', 'utf-16le', 'JIS', 'SJIS', 'eucjp-win', 'sjis-win', 'gbk');
         $enc = '';
 
-        if (function_exists("mb_detect_encoding") && $enc = @mb_detect_encoding($string, $list, true)) return $enc;
-
         // Unicode BOM is U+FEFF, but after encoded, it will look like this.
         $UTF32_BIG_ENDIAN_BOM       = chr(0x00) . chr(0x00) . chr(0xFE) . chr(0xFF);
         $UTF32_LITTLE_ENDIAN_BOM    = chr(0xFF) . chr(0xFE) . chr(0x00) . chr(0x00);
@@ -276,6 +274,9 @@ class Base {
                 }
             }
         }
+
+        if (!$enc && function_exists("mb_detect_encoding")) $enc = @mb_detect_encoding($string, $list, true);
+
         return $enc;
     }
 
